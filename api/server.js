727604da0775840f3721fb288d822f72c09e1117
thuan.nv <nguyenthuankma@gmail.com>
2024-03-04 15:57:32 +0700

    Upload

--- api/server.js
+++ api/server.js
@@ -1,19 +1,32 @@
-// See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require('json-server');
+const express = require('express');
+const path = require('path');
 
-server.use(middlewares)
-// Add this before server.use(router)
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+// Tạo một ứng dụng Express mới
+const app = express();
+
+// Sử dụng các middleware mặc định của JSON Server
+app.use(middlewares);
+
+// Serve static files từ thư mục 'uploads'
+app.use('../uploads', express.static(path.join(__dirname, 'uploads')));
+
+// Rewrite rules
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
-server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+}));
 
-// Export the Server API
-module.exports = server
+// Sử dụng router
+app.use(router);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`JSON Server is running on port ${port}`);
+});
+
+module.exports = server;
